c359c3411e74634b1cc95c6d2d001dfc896c07af
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-11-12 23:01:22 +0900

    ✨ tablayout, viewpager2 적용

--- app/src/main/java/com/example/pokemonencyclopedia/MainActivity.kt
+++ app/src/main/java/com/example/pokemonencyclopedia/MainActivity.kt
@@ -1,27 +1,25 @@
 package com.example.pokemonencyclopedia
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pokemonencyclopedia.adapter.ViewPagerAdapter
 import com.example.pokemonencyclopedia.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentTitleList = mutableListOf("관동","성도","호연","신오","하나","칼로스","알로라","가라르","히스이")
+    private val fragmentIconList = mutableListOf(R.drawable.kanto,R.drawable.saint,R.drawable.hoenn,R.drawable.shino,R.drawable.hana,R.drawable.carlos,R.drawable.alola,R.drawable.galar,R.drawable.hisui)
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.kantoMedal.setOnClickListener {
-            startActivity(Intent(this, KantoActivity::class.java))
-        }
-
-        binding.saintMedal.setOnClickListener {
-            startActivity(Intent(this, SaintActivity::class.java))
-        }
-
-        binding.hoennMedal.setOnClickListener {
-            startActivity(Intent(this, HoennActivity::class.java))
-        }
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(binding.localTabLayout, binding.viewPager) { tab, pos ->
+            tab.text = fragmentTitleList[pos]
+            tab.setIcon(fragmentIconList[pos])
+        }.attach()
     }
 }
